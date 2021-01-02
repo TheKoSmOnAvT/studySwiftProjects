020e007c2cdba5462b259b5a03b2cbcc3dc7b51c
TheKoSmOnAvT <ni_popov@mail.ru>
2021-01-02 17:51:14 +0500

    end 7th video

--- standfordEmojiArt/emojiArt/emojiArt/EmojiArt.swift
+++ standfordEmojiArt/emojiArt/emojiArt/EmojiArt.swift
@@ -9,4 +9,27 @@ import Foundation
 
 struct EmojiArt {
     var backgroundURL : URL?
+    var emojis = [Emoji]()
+    
+    struct Emoji : Identifiable {
+        let id : Int
+        let text : String
+        var x : Int
+        var y : Int
+        var size : Int
+        
+        init(id: Int, text: String, x: Int, y: Int, size: Int) {
+            self.id = id
+            self.text = text
+            self.x = x
+            self.y = y
+            self.size = size
+        }
+    }
+    
+    private var uniqueEmojiId = 0
+    mutating func addEmoji(text : String, x : Int, y : Int, size : Int){
+        self.emojis.append(Emoji(id: uniqueEmojiId, text: text, x: x, y: y, size: size ))
+        self.uniqueEmojiId = uniqueEmojiId + 1
+    }
 }
--- standfordEmojiArt/emojiArt/emojiArt/EmojiArtDocumentView.swift
+++ standfordEmojiArt/emojiArt/emojiArt/EmojiArtDocumentView.swift
@@ -16,15 +16,56 @@ struct EmojiArtDocumentView: View {
                     ForEach(EmojiArtDocument.palette.map{String($0)}, id : \.self)  { emoji in
                         Text(emoji)
                             .font(Font.system(size: self.defaultEmojiSize))
+                            .onDrag{ return NSItemProvider(object : emoji as NSString) }
                     }
                 }
             }
             .padding(.horizontal)
-            Rectangle().foregroundColor(Color.blue)
-                .edgesIgnoringSafeArea([.horizontal, .bottom])
+            GeometryReader {  geometry  in
+                ZStack {
+                    Rectangle().foregroundColor(Color.blue).overlay(
+                        Group {
+                            if self.document.backgroundImgage  != nil {
+                                Image(uiImage : self.document.backgroundImgage!)
+                            }
+                        }
+                    )
+                        .edgesIgnoringSafeArea([.horizontal, .bottom])
+                        .onDrop(of: ["public.image", "public.text"], isTargeted: nil) { provaders, location  in
+                            var location = geometry.convert(location,  from: .global)
+                            location  =  CGPoint(x: location.x  - geometry.size.width/2, y: location.y - geometry.size.height/2)
+                            return self.drop(provaders: provaders, at : location)
+                    }
+                    ForEach(self.document.emojis) { emoji in
+                        Text(emoji.text)
+                            .font(self.font(for: emoji))
+                            .position(self.position(for: emoji, in: geometry.size))
+                            
+                    }
+                }
+            }
         }
     }
     
+    private func font(for emoji  : EmojiArt.Emoji)  ->  Font{
+        Font.system(size: emoji.fontSize)
+    }
+    
+    private func position(for emoji  : EmojiArt.Emoji, in size  :  CGSize )  -> CGPoint {
+        CGPoint(x: emoji.location.x + size.width/2, y: emoji.location.y + size.height/2)
+    }
+    
+    private func drop(provaders: [NSItemProvider], at location : CGPoint) -> Bool {
+        var found = provaders.loadFirstObject(ofType: URL.self) { url in
+            self.document.setBackGroundURL(url)
+        }
+        if !found {
+            found = provaders.loadObjects(ofType: String.self) {  string in
+                self.document.addEmoji(string, at: location, size: defaultEmojiSize)
+            }
+        }
+        return found
+    }
     private let defaultEmojiSize : CGFloat  = 30
 }
 
