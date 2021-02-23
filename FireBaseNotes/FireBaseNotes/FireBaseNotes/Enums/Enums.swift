09b39f3dc8d043146f88f917f394fcb376735b5d
TheKoSmOnAvT <ni_popov@mail.ru>
2021-02-23 22:44:12 +0500

    end registration

---
+++ FireBaseNotes/FireBaseNotes/FireBaseNotes/Enums/Enums.swift
@@ -0,0 +1,12 @@
+//
+//  Enums.swift
+//  FireBaseNotes
+//
+//  Created by Никита Попов on 23.02.2021.
+//
+
+import Foundation
+
+public enum DefaultProfile : String {
+    case authId  = "authId"
+}
--- FireBaseNotes/FireBaseNotes/FireBaseNotes/ModelView/FireBase.swift
+++ FireBaseNotes/FireBaseNotes/FireBaseNotes/ModelView/FireBase.swift
@@ -7,6 +7,10 @@
 
 import Foundation
 import Firebase
+import Foundation
+import var CommonCrypto.CC_MD5_DIGEST_LENGTH
+import func CommonCrypto.CC_MD5
+import typealias CommonCrypto.CC_LONG
 
 public class FireBase {
     
@@ -25,10 +29,9 @@ public class FireBase {
                    if let id = snapshot.value  {
                     if("\(id)" == "<null>") {
                             let id = UUID().uuidString
-                            var hash  = Hasher()
-                            hash.combine(password)
-                            let resultHash  =  hash.finalize()
-                            self.reference.child("users").child(mail).setValue(["id" : id,  "password" : resultHash])
+                            let hash  =  self.MD5(string: password)
+                            let hashString = hash.map { String(format: "%02hhx", $0) }.joined()
+                            self.reference.child("users").child(mail).setValue(["id" : id,  "password" : String(hashString)])
                             result = id
                         }
                    }
@@ -37,31 +40,52 @@ public class FireBase {
     }
     
     func login(mail : String, password  : String) {
-        var hash  = Hasher()
-        hash.combine(password)
-        let resultHash  =  hash.finalize()
+        let hash  =  MD5(string: password)
+        let hashString = hash.map { String(format: "%02hhx", $0) }.joined()
         self.reference.child("users").child(mail).observeSingleEvent(of: .value, with: { (snapshot) in
-            
             if let result = snapshot.value as? [String : Any] {
-                print("2222")
-                    print(result["password"])
-                print("222")
-                
+                if let pass = result["password"] as? String, let id = result["id"] as? String  {
+                    if (pass  ==  hashString){
+                        UserDefaults.standard.setValue(id, forKey: DefaultProfile.authId.rawValue)
+                    }
+               }
             }
-            
-                print("123")
         })
     }
     
+    func logout() {
+        UserDefaults.standard.setValue(nil, forKey: DefaultProfile.authId.rawValue)
+    }
     
     
     func appendNote(title : String, text :  String){
         let id = UUID().uuidString
         self.reference.child("Note").child(id).setValue(["title" : title,  "text" :text])
     }
+    
+    
+   private func MD5(string: String) -> Data {
+            let length = Int(CC_MD5_DIGEST_LENGTH)
+            let messageData = string.data(using:.utf8)!
+            var digestData = Data(count: length)
+
+            _ = digestData.withUnsafeMutableBytes { digestBytes -> UInt8 in
+                messageData.withUnsafeBytes { messageBytes -> UInt8 in
+                    if let messageBytesBaseAddress = messageBytes.baseAddress, let digestBytesBlindMemory = digestBytes.bindMemory(to: UInt8.self).baseAddress {
+                        let messageLength = CC_LONG(messageData.count)
+                        CC_MD5(messageBytesBaseAddress, messageLength, digestBytesBlindMemory)
+                    }
+                    return 0
+                }
+            }
+            return digestData
+        }
+    
+    
 }
 
     
+
     
     
 //    func  getData() {
